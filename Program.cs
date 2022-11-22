4cdf6679e654b89bb133f195e51ea6204ab8a7f2
ElenaM <elenam108@yandex.ru>
2022-11-22 20:18:59 +0300

    new

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.WriteLine("Введите натуральное число больше 1:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+void NumberCounter (int N)
+{
+    if (N == 0) return;
+    Console.Write("{0,4}", N);
+    NumberCounter (N - 1);
+}
+NumberCounter(N);
